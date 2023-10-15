d720363711e30141968570f3efdcfac02d5ac268
pranto0000 <125544914+pranto0000@users.noreply.github.com>
2023-10-15 19:06:53 +0600

    Add files via upload

---
+++ models/emailVerification.js
@@ -0,0 +1,25 @@
+import mongoose, { Schema } from "mongoose";
+
+const emailVerificationSchema = Schema({
+  userId: String,
+  token: String,
+  createdAt: Date,
+  expiresAt: Date,
+});
+
+const Verification = mongoose.model("Verification", emailVerificationSchema);
+
+export default Verification;
+
+
+// import mongoose,{Schema} from "mongoose";
+
+// const emailVerificationSchema = Schema({
+//     userId:String,
+//     token:String,
+//     createdAt:Date,
+//     expiresAt:Date,
+// })
+
+// const Verification = mongoose.model('Verification', emailVerificationSchema);
+// export default Verification
